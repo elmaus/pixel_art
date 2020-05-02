baeacdf27a74e170d75780ed54accdf7a707ffda
elmaus <42428075+elmaus@users.noreply.github.com>
2020-05-02 21:35:31 +0300

    Update pad.py

--- pad.py
+++ pad.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from PIL import Image
+import numpy as np
 
 
 # ListPad(root, content, args[padx, pady, width, anchor, bd]
@@ -19,12 +21,21 @@ class Pixel:
         self.parent.create_rectangle(self.row, self.col, self.row + 10, self.col + 10, fill=self.fill, outline=self.outline)
 
 
+class Welcome(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, bg='grey')
+
+
+        self.text = 'Welcome to my Pixel Art Application...\n\nThis is my first attempt to write pixel art application'
+
+        self.greet = tk.Label(self, text=self.text)
+        self.greet.pack()
 
 class Pad(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, args[0], bg='grey')
 
-
+        self.parent = args[0]
         self.name = kwargs['name']
 
         self.pixels = [] # list of all pixels
@@ -34,9 +45,17 @@ class Pad(tk.Frame):
         self.ai = [-10, 0, +10, 0]
         self.aj = [0, +10, 0, -10]
 
+
+        self.pixels_in_row = kwargs['height']
+        self.pixels_in_col = kwargs['width']
+        self.pixel_width = 10
+        self.canvas_width = self.pixels_in_col * self.pixel_width
+        self.canvas_height = self.pixels_in_row * self.pixel_width
+
+
         self.frame_main = tk.Canvas(self)
 
-        self.main_canvas = tk.Canvas(self.frame_main, width=800, height=600)
+        self.main_canvas = tk.Canvas(self.frame_main, width=self.canvas_width, height=self.canvas_height)
 
         self.vbar = tk.Scrollbar(self, orient='vertical', command=self.main_canvas.yview)
         self.hbar = tk.Scrollbar(self, orient='horizontal', command=self.main_canvas.xview)
@@ -48,7 +67,7 @@ class Pad(tk.Frame):
         self.hbar.pack(side='bottom', anchor='sw', fill='x')
         self.frame_main.pack(side='left')
 
-        self.main_canvas.pack(side='top', expand='yes')
+        self.main_canvas.pack(side='top', anchor='n')
 
 
         self.scroll_frame = tk.Frame(self.main_canvas)
@@ -57,33 +76,52 @@ class Pad(tk.Frame):
             scrollregion=self.main_canvas.bbox("all")))
         self.main_canvas.create_window((0, 0), window=self.scroll_frame, anchor="nw")
 
-        self.canvas = tk.Canvas(self.scroll_frame, width=800, height=500, bg='grey')
+        self.canvas = tk.Canvas(self.scroll_frame, width=self.canvas_width, height=self.canvas_height, bg='grey')
         self.canvas.pack(side='top', anchor='nw')
+        self.canvas.bind('<B1-Motion>', self.motion)
 
         self.CreatePixel()
 
-    def CreatePixel(self):
 
-        ''' Creating pixel canvas '''
-        row = 2
-        for i in range(800):
-            rows = []
-            col = 2
-            for j in range(600):
-                pix = self.canvas.create_rectangle(row, col, row+10, col+10, fill='white', outline='dark grey')
-                self.canvas.tag_bind(pix, '<Button-1>', self.tap)
-                rows.append(pix)
-                col += 10
-            row += 10
-            self.pixels.append(rows)
+    def export(self, name, ext):
 
+        ''' exporting pixel value into image '''
 
-    def tap(self, event):
+        pixels = []
+        for i in range(self.pixels_in_row):
+            row = []
+            for j in range(self.pixels_in_col):
+                item = self.pixels[i][j]
+                hex = self.canvas.itemcget(item, 'fill').lstrip('#')
+                if ext == 'png':
+                    if self.canvas.itemcget(item, 'outline') == 'dark grey':
+                        v = tuple(int(hex[x:x+2], 16) for x in (0, 2, 4))
+                        vlist = list(v)
+                        vlist.append(0)
+                        tup = tuple(vlist)
+                        row.append(tup)
+                    else:
+                        v = tuple(int(hex[x:x + 2], 16) for x in (0, 2, 4))
+                        vlist = list(v)
+                        vlist.append(255)
+                        tup = tuple(vlist)
+                        row.append(tup)
+                else:
+                    v = tuple(int(hex[x:x + 2], 16) for x in (0, 2, 4))
+                    row.append(v)
 
-        ''' coloring the selected pixel '''
+            pixels.append(row)
 
-        x = event.x//10
-        y = event.y//10
+        array = np.array(pixels, dtype=np.uint8)
+        new_image = Image.fromarray(array)
+        new_image.save('{}.png'.format(name))
+
+    def motion(self, event):
+
+        ''' drawing on canvas while mouse on press '''
+
+        x = (event.x + 2) // self.pixel_width
+        y = (event.y + 2) // self.pixel_width
 
         pixel = int(float(pen_size.get()))
 
@@ -91,8 +129,53 @@ class Pad(tk.Frame):
         pixels = []
         for i in range(pixel):
             for j in range(pixel):
-                pixels.append(self.pixels[i + x][j + y])
+                try:
+                    pixels.append(self.pixels[i + x][j + y])
+                except:
+                    pass
 
+        if tools == 'brush':
+            for p in pixels:
+                self.canvas.itemconfigure(p, fill=selected_color, outline=selected_color)
+
+        if tools == 'erase':
+            for p in pixels:
+                self.canvas.itemconfigure(p, fill='white', outline='dark grey')
+
+    def CreatePixel(self):
+
+        ''' Creating pixel canvas '''
+        row = 2
+        for i in range(self.pixels_in_col):
+            rows = []
+            col = 2
+            for j in range(self.pixels_in_row):
+                pix = self.canvas.create_rectangle(row, col, row + self.pixel_width, col + self.pixel_width, fill='#ffffff', outline='dark grey')
+                self.canvas.tag_bind(pix, '<Button-1>', self.tap)
+                rows.append(pix)
+                col += self.pixel_width
+            row += self.pixel_width
+            self.pixels.append(rows)
+
+
+    def tap(self, event):
+
+        ''' coloring the selected pixel '''
+
+        x = (event.x - 2) // self.pixel_width
+        y = (event.y - 2) // self.pixel_width
+
+        pixel = int(float(pen_size.get()))
+
+        # collecting all pixels included based on the size of the pen
+        pixels = []
+        if tools != 'paint':
+            for i in range(pixel):
+                for j in range(pixel):
+                    try:
+                        pixels.append(self.pixels[i + x][j + y])
+                    except:
+                        pass
         if tools == 'brush':
             for p in pixels:
                 self.canvas.itemconfigure(p, fill=selected_color, outline=selected_color)
@@ -104,7 +187,7 @@ class Pad(tk.Frame):
         if tools == 'paint':
             item = self.pixels[x][y]
             self.color_recursion = self.canvas.itemcget(item, "fill")
-            self.paint(self.color_recursion, item, y, x)
+            self.paint(self.color_recursion, item, x, y)
 
 
     def paint(self, color, item, row, col):
@@ -114,17 +197,51 @@ class Pad(tk.Frame):
         if color == self.color_recursion:
             self.canvas.itemconfigure(item, fill=selected_color, outline=selected_color)
             for i, j, in zip(self.ar, self.ac):
-                r = row + j
-                c = col + i
-                if c >=0 and c + j <= 100 and r + i >= 0 and r + i <= 100:
+                r = row + i
+                c = col + j
+                if c >=0 and c <= self.pixels_in_col - 1 and r >= 0 and r <= self.pixels_in_row - 1:
                     p = self.pixels[c][r]
                     p_color = self.canvas.itemcget(p, 'fill')
-                    # self.canvas.itemconfigure(p, fill='red', outline=selected_color)
                     self.paint(p_color, p, r, c)
                 else:
                     return
         else:
             return
+
+    def zoom(self, z):
+
+        ''' zooming in and out of the pad '''
+
+        if z == '+' and self.pixel_width < 30:
+            self.pixel_width += 4
+            self.canvas_width = self.pixels_in_col * self.pixel_width
+            self.canvas_height = self.pixels_in_row * self.pixel_width
+            self.canvas.configure(width=self.canvas_width, height=self.canvas_height)
+
+            row = 2
+            for i in range(self.pixels_in_col):
+                col = 2
+                for j in range(self.pixels_in_row):
+                    item = self.pixels[i][j]
+                    self.canvas.coords(item, row, col, row + self.pixel_width, col + self.pixel_width)
+                    col += self.pixel_width
+                row += self.pixel_width
+
+        if z == '-' and self.pixel_width > 10:
+            self.pixel_width -= 4
+            self.canvas_width = self.pixels_in_col * self.pixel_width
+            self.canvas_height = self.pixels_in_row * self.pixel_width
+            self.canvas.configure(width=self.canvas_width, height=self.canvas_height)
+
+            row = 2
+            for i in range(self.pixels_in_col):
+                col = 2
+                for j in range(self.pixels_in_row):
+                    item = self.pixels[i][j]
+                    self.canvas.coords(item, row, col, row + self.pixel_width, col + self.pixel_width)
+                    col += self.pixel_width
+                row += self.pixel_width
+
 
 
 
@@ -137,6 +254,7 @@ class DrawingPad(ttk.Notebook):
         self.maseter = args[0]
         selected_color = args[1]
         pen_size = args[2]
+        self.tab_dicionary = {}
 
         if not self.__initialized:
             self.__initialize_custom_style()
@@ -177,8 +295,11 @@ class DrawingPad(ttk.Notebook):
             # removing tab in the window
             self.hide(index)
             # removing tab in the opened tab dictionary
-            del self.tab_dict[index]
-            self.event_generate("<<NotebookTabClosed>>")
+            try:
+                del self.tab_dict[index]
+                self.event_generate("<<NotebookTabClosed>>")
+            except:
+                pass
 
         self.state(["!pressed"])
         self._active = None
@@ -228,13 +349,19 @@ class DrawingPad(ttk.Notebook):
         })
     ])
 
-    def add_tab(self):
+    def add_welcome(self):
+        pad = Welcome()
+        self.add(pad, text='Welcome')
+        self.tab_dicionary['welcome'] = pad
+
+    def add_tab(self, name, w, h):
 
         ''' creating new empty tab '''
 
-        pad = Pad(self.master, name='New')
-        pad.pack(side='top', anchor='nw', expand='yes', fill='both')
-        self.add(pad, text='New')
+        pad = Pad(self, name='New', width=w, height=h)
+        pad.pack(side='top')
+        self.add(pad, text=name)
+        self.tab_dicionary[name] = pad
 
 
     def change_color(self, color):
@@ -255,4 +382,23 @@ class DrawingPad(ttk.Notebook):
 
         global tools
         tools = tool
+
+
+    def zoom_in(self, z):
+
+        ''' zooming in the pad '''
+
+        name = self.tab(self.active_tab)['text']
+        tab = self.tab_dicionary[name]
+        tab.zoom(z)
+
+
+    def export(self, ext):
+
+        ''' exporting file into image '''
+
+        name = self.tab(self.active_tab)['text']
+        tab = self.tab_dicionary[name]
+        tab.export(name, ext)
+
 
